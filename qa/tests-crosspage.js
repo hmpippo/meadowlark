069fb057306d0bc0ec070cafe8777365d921736b
Hui Min, Liu <a60695420@sina.com>
2017-06-04 13:20:13 +0800

    Integrate with Travis CI2

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -31,12 +31,12 @@ suite('Cross-Page Tests', function () {
        });
     });
     */
-/*
+
     test('visiting the "request group rate" page directly should result in an empty referrer field', function (done) {
        browser.visit('http://localhost:3000/tours/request-group-rate', function () {
           assert(browser.field('referrer').value === '');
           done();
        });
     });
-*/
+
 });
